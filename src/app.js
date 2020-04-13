d159195517063c9e587177debb829702caa66a2d
Carlos H <carlos@3core.io>
2020-04-13 20:55:40 -0300

    conclusao desafio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,75 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  if (repositories.length > 0 )
+    response.json(repositories);
+  else
+    response.status(204).send();
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  var repository = {
+    id: uuid(),
+    title: title,
+    url: url,
+    techs: techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const id = request.params.id;
+  const { title, url, techs } = request.body;
+
+  let repositoryIndex = repositories.findIndex(repository => repository.id === id );
+
+  if (repositoryIndex < 0)
+    response.status(400).json({ message: `repository '${id}' not found` });
+
+  var repository = {
+    id: repositories[repositoryIndex].id,
+    title: title,
+    url: url,
+    techs: techs,
+    likes: repositories[repositoryIndex].likes
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const id = request.params.id;
+  let repositoryIndex = repositories.findIndex(repository => repository.id === id );
+
+  if (repositoryIndex < 0)
+    response.status(400).json({ message: `repository '${id}' not found` });
+
+  repositories.splice(repositoryIndex, 1);
+
+  response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const id = request.params.id;
+  let repositoryIndex = repositories.findIndex(repository => repository.id === id );
+
+  if (repositoryIndex < 0)
+    response.status(400).json({ message: `repository '${id}' not found` });
+
+  var repository = repositories[repositoryIndex];
+  repository.likes++;
+  repositories[repositoryIndex] = repository;
+
+  response.json({ likes: repository.likes });
+
 });
 
 module.exports = app;
